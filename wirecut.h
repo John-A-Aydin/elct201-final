966acb01b7f44e8f00a9ac440a77cf41446a9b2b
tylerbeasley55 <tbeasley@email.sc.edu>
2024-04-09 03:41:38 +0000

    some stuff
    Signed-off-by: tylerbeasley55 <tbeasley@email.sc.edu>

--- wirecut.h
+++ wirecut.h
@@ -13,7 +13,49 @@ int wireCount = 4;
 int wirecut() {
    int bombState = 1;
    while(bombState == 1) {
-       //yeah
+       int code = 4;
+       int current = 0;
+       if(current > 4 ) bombState = 0;
+       if(blackWire == 0) {
+           if (current == 0) {
+               current++;
+           } 
+           else if (current > 0 && current < 5) {
+               break;
+           } else {
+               current += 5;
+           }
+       }
+       if(greenWire == 0) {
+           if (current == 1) {
+               current++;
+           } 
+           else if (current > 1 && current < 5 ) {
+               break;
+           } else {
+               current += 5;
+           }
+       }
+      if(redWire == 0) {
+           if (current == 2) {
+               current++;
+           } 
+           else if (current > 2 && current < 5 ) {
+               break;
+           } else {
+               current += 5;
+           }
+       }
+       if(purpleWire == 0) {
+           if (current == 3) {
+               current++;
+           } 
+           else if (current == 4 ) {
+               return bombState;
+           } else {
+               current += 5;
+           }
+       }
    } 
    return bombState;
 }
