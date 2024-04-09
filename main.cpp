0beb2252e23f45e168beea4ef81e99c0a880f346
tylerbeasley55 <tbeasley@email.sc.edu>
2024-04-09 22:02:36 +0000

    working wirecut
    Signed-off-by: tylerbeasley55 <tbeasley@email.sc.edu>

--- main.cpp
+++ main.cpp
@@ -4,19 +4,29 @@
  */
 
 #include "mbed.h"
+#include <cstdio>
+#include <iostream>
+#include "wirecut.h"
 
 
 // Blinking rate in milliseconds
 #define BLINKING_RATE     500ms
 
-
 int main()
 {
     // Initialise the digital pin LED1 as an output
     DigitalOut led(LED1);
-
     while (true) {
         led = !led;
-        ThisThread::sleep_for(BLINKING_RATE);
+        wirecut();
+        /* std::cout << "red: " << redWire.read() << std::endl;
+        std::cout << "blue: " <<  blueWire.read() << std::endl;
+        std::cout << "green: " <<  greenWire.read() << std::endl;
+        std::cout << "purple: " << purpleWire.read() << std::endl;
+        std::cout <<  std::endl;
+        std::cout << std::endl;
+        std::cout << std::endl;
+        wait_us(5000000); */
+        std::cout << bombState << std::endl;
     }
 }
--- wirecut.h
+++ wirecut.h
@@ -2,62 +2,68 @@
 #define WIRECUT_H_
 
 #include "mbed.h"
+#include <iostream>
 #include <vector>
 #include <string>
 
 InterruptIn redWire(D1);
-InterruptIn blackWire(D2);
-InterruptIn purpleWire(D3);
-InterruptIn greenWire(D4);
-int wireCount = 4;
-int wirecut() {
-   int bombState = 1;
+InterruptIn blueWire(D2);
+InterruptIn purpleWire(D4);
+InterruptIn greenWire(D3);
+int bombState = 1;
+void wirecut() {
+    // cout << "echo" << endl;
    while(bombState == 1) {
        int code = 4;
        int current = 0;
-       if(current > 4 ) bombState = 0;
-       if(blackWire == 0) {
+       if(blueWire.read() == 0) {
            if (current == 0) {
                current++;
            } 
            else if (current > 0 && current < 5) {
-               break;
+                ;
            } else {
                current += 5;
            }
        }
-       if(greenWire == 0) {
+       if(greenWire.read() == 0) {
            if (current == 1) {
                current++;
            } 
            else if (current > 1 && current < 5 ) {
-               break;
+                ;
            } else {
                current += 5;
            }
        }
-      if(redWire == 0) {
+      if(redWire.read() == 0) {
            if (current == 2) {
                current++;
            } 
            else if (current > 2 && current < 5 ) {
-               break;
+                ;
            } else {
                current += 5;
            }
        }
-       if(purpleWire == 0) {
+       if(purpleWire.read() == 0) {
            if (current == 3) {
                current++;
            } 
            else if (current == 4 ) {
-               return bombState;
+               ;
            } else {
                current += 5;
            }
        }
+       if(current > 4) {
+           bombState = 0;
+           break;
+       }
+       if(current == 4) {
+           break;
+       }
    } 
-   return bombState;
 }
 
 
